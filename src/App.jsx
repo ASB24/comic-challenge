a7550d7234a1bf803e6e40b349c6e744487ecb01
Alex Serafini <alexserafini2001@gmail.com>
2022-12-09 15:58:13 -0400

    add front-app for project

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,80 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
+import Modal from 'react-modal'
 import './App.css'
 
+const API_URL = "http://localhost:8000"
+const queryClient = new QueryClient()
+Modal.setAppElement('#root')
+
+
+const GetIssues = () => {
+  const { isLoading, error, data } = useQuery('issuesData', () =>
+     fetch(`${API_URL}/issues?sort=date_added:asc`).then(res =>
+       res.json()
+     )
+   )
+
+   if(isLoading) return 'Cargando...'
+
+   if(error) return `Ocurrió un error ${error.message}`
+
+   let issues = data.results
+
+   return(
+    <div className='issues-list'>
+        {issues.filter(i => i.description !== null && i.description !== "<br />").map((i) => {
+          return <SpawnModal issue={i}></SpawnModal>
+        })}
+    </div>
+   )
+}
+
+const SpawnModal = (props) => {
+  const issue = props.issue
+  //console.log(this.props.issue.cover_date)
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  let openModal = () => {
+    console.log(issue.id)
+    setIsOpen(true)
+  }
+  let closeModal = () => setIsOpen(false)
+
+  const style = {
+    content: {
+      backgroundImage:"linear-gradient(to right, #242424 0%, black 100%)",
+    }
+  }
+
+  return(
+    <>
+    <div onClick={() => openModal()} className="card" style={{backgroundImage:`url("${issue.image.super_url}")`, boxShadow:"0 0 50px rgba(0, 0, 0, 0.515)"}}></div>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={style}
+        contentLabel={props.issue.cover_date}
+        >
+        <img src={issue.image.screen_url} />
+        <h1>{issue.name ?? "El api no provee un nombre"}</h1>
+        <p dangerouslySetInnerHTML={{__html: issue.description ?? issue.deck ?? "No tiene descripcion"}}></p>
+        <span><i className="fa-sharp fa-solid fa-calendars"></i>{new Date(issue.cover_date).toLocaleDateString() ?? "No tiene fecha de publicacion"}</span>
+      </Modal>
+    </>
+  )
+}
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <QueryClientProvider client={queryClient}>
+          <GetIssues />
+        </QueryClientProvider>
     </div>
   )
+
 }
 
 export default App
